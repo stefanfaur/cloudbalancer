00f509964aa8d7c478704973aab1babf02f2a3ca
sfaur <stefan.faur@irian.ro>
2026-03-20 09:17:52 +0200

    feat(build): add Spring Boot and Spring Kafka dependencies for dispatcher and worker

--- dispatcher/build.gradle.kts
+++ dispatcher/build.gradle.kts
@@ -1,3 +1,15 @@
+plugins {
+    alias(libs.plugins.spring.boot)
+}
+
 dependencies {
     implementation(project(":common"))
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.kafka)
+
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.spring.boot.webmvc.test)
+    testImplementation(libs.testcontainers.core)
+    testImplementation(libs.testcontainers.junit)
+    testImplementation(libs.testcontainers.kafka)
 }
--- worker/build.gradle.kts
+++ worker/build.gradle.kts
@@ -1,3 +1,14 @@
+plugins {
+    alias(libs.plugins.spring.boot)
+}
+
 dependencies {
     implementation(project(":common"))
+    implementation(libs.spring.boot.starter)
+    implementation(libs.spring.boot.starter.kafka)
+
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.testcontainers.core)
+    testImplementation(libs.testcontainers.junit)
+    testImplementation(libs.testcontainers.kafka)
 }
